2ad181293931f233ad6865acc86de0749cd2ba2e
stephanygranadaboloy18 <stephanygranadaboloy18@gmail.com>
2026-04-23 21:44:11 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+function postAnnouncement() {
+    // get input value
+    var message = document.getElementById("messageInput").value;
+
+    // display it in output box
+    document.getElementById("outputBox").innerText = message;
+}
